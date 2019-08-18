79a42a833baed5760f120eb4516cf6d70c4cbb0a
Jim <jim@advancebass.com>
2019-08-18 12:34:33 -0700

    add some pew and pop sounds, experimenting with bullet collision to transform the bullet into something else, at the moment it's scaling to be larger and chaning vector

--- Assets/Scripts/Bullet.cs
+++ Assets/Scripts/Bullet.cs
@@ -10,35 +10,52 @@ public class Bullet : MonoBehaviour
 
   public Rigidbody rig;
 
+  public AudioClip shootSound;
+  public AudioClip collisionSound;
+  public AudioSource audio;
+
+  void Start()
+  {
+    audio = GetComponent<AudioSource>();
+  }
+
   public void Initialize(int damage, int attackerId, bool isMine)
   {
     this.damage = damage;
     this.attackerId = attackerId;
     this.isMine = isMine;
 
-    Destroy(gameObject, 10.0f);
+    audio.PlayOneShot(shootSound, 0.50f);
+
+    // time until destroy
+    Destroy(gameObject, 42.0f);
   }
 
   void OnTriggerEnter(Collider other)
   {
     if (other.CompareTag("Player") && isMine)
     {
+      audio.PlayOneShot(collisionSound, 0.50f);
       PlayerController player = GameManager.instance.GetPlayer(other.gameObject);
 
       if (player.id != attackerId)
         player.photonView.RPC("TakeDamage", player.photonPlayer, attackerId, damage);
     }
 
-    // if (other.gameObject.name == "Terrain")
-    // {
-    //     Debug.Log("Terrain Hit");
-    //     rig.velocity = new Vector3(0,1,0);
-    //     // rig.transform.forward =  new Vector3(0,1,0);
-    // }
-    // else
-    // {
-    Destroy(gameObject);
-    // }
+    if (other.gameObject.name == "Terrain")
+    {
+      audio.PlayOneShot(collisionSound, 0.50f);
+      // Debug.Log("Terrain Hit");
+      rig.velocity = new Vector3(0, 1, 0);
+
+      // change scale of bullet
+      gameObject.transform.localScale += new Vector3(1, 1, 1);
+      // rig.transform.forward =  new Vector3(0,1,0);
+    }
+    else
+    {
+      Destroy(gameObject);
+    }
 
 
   }
